c7e4ca1b48f8eb2363a1f18a3570bc3ecf030bb5
Amy Jones <amyjprogrammer@gmail.com>
2021-01-31 03:04:35 -0500

    Commit new html and views

--- pet_health_tracker/urls.py
+++ pet_health_tracker/urls.py
@@ -5,9 +5,11 @@ from django.urls import path
 from . import views
 
 app_name = 'pet_health_tracker'
+
 urlpatterns = [
     path('', views.home ,name="home"),
     path('pet_names/', views.pet_names, name='pet_names'),
-    path('pet_names/<int:pet_id>', views.pet_health, name='pet_health'),
-
+    path('pet_health/<int:pet_id>/', views.pet_health, name='pet_health'),
+    path('pet_form/<int:pet_id>/', views.pet_form, name='pet_form'),
+    path('add_pet/<int:pet_id>/', views.add_pet, name='add_pet'),
     ]
--- pet_health_tracker/views.py
+++ pet_health_tracker/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 from .models import PetInfo, HealthTracker
 from .forms import PetInfoForm, HealthTrackerForm
@@ -7,12 +9,37 @@ from .forms import PetInfoForm, HealthTrackerForm
 def home(request):
     return render(request, 'pet_health_tracker/home.html')
 
+@login_required
 def pet_names(request):
     """page showing all the pet names"""
     pet_names= PetInfo.objects.filter(owner=request.user).order_by('pet_name')
     context = {'pet_names': pet_names}
     return render(request, 'pet_health_tracker/pet_names.html', context)
 
-def pet_health(response, pet_id):
+@login_required
+def add_pet(request, pet_id):
+    """form to add a pet"""
+
+    return render(request, 'pet_health_tracker/add_pet.html', context)
+
+
+@login_required
+def pet_health(request, pet_id):
     """Show a single pet and all the info"""
-    pet_name = PetInfo.objects.get(id=pet_id)
+    pet_name = get_object_or_404(PetInfo, id=pet_id)
+    check_pet_owner(request, pet_name.owner)
+    health_trackers = pet_name.healthtracker_set.order_by('date_added')
+    context = {'pet_name': pet_name, 'health_trackers': health_trackers}
+    return render(request, 'pet_health_tracker/pet_health.html', context)
+
+@login_required
+def pet_form(request, pet_id):
+    """option to add a new health form"""
+
+    return render(request, 'pet_health_tracker/pet_form.html', context)
+
+@login_required
+def check_pet_owner(request, owner):
+    """makes sure editor or viewer is the owner"""
+    if owner!= request.user:
+        raise Http404
